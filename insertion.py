9d6239f3a77fab7f842c1f35ef2faa995459c5b8
Anthony Nguyen <nguyean6@flip3.engr.oregonstate.edu>
2020-01-12 17:02:44 -0800

    update

--- insertion.py
+++ insertion.py
@@ -11,16 +11,43 @@ def main():
     array = []
     for j in range(len(line)):
         temp = [int(i) for i in line[j].split() if i.isdigit()]
+        print(temp)
         insertionsort(temp)
         array.append(temp)
 
-    print(array)
+    #print(array)
     #insertionsort(array)
+    out_file(array)
 
 def insertionsort(array):
-    for i in range(1,len(array)):
-        for j in range(i,0,-1):
-            if array[j] <= array[j-1]:
-                array[j], array[j-1] = array[j-1], array[j]
+    # bubble sort
+    # for i in range(1,len(array)):
+    #     for j in range(i,0,-1):
+    #         if array[j] <= array[j-1]:
+    #             array[j], array[j-1] = array[j-1], array[j]
+    #insertion sort
+    for i in range(1, len(array)):
+        current = i
+        while(array[i] <= array[current-1]):
+            current -= 1
+            if(current==0):
+                break
+        array.insert(current, array[i])
+        array.pop(i+1)
+
+    #print(array)
+
+def out_file(array):
+    out = open("output.txt", "w")
+    out.truncate(0)
+    for i in range(len(array)):
+        write = ""
+        for number in array[i]:
+            write += str(number) + " "
+        write += "\n"
+        out.write(write)
+    #print(write)
+    out.close()
+
 
 main()
--- mergesort.py
+++ mergesort.py
@@ -6,12 +6,17 @@ def main():
     data.close()
     #print(line)
     array = []
+    # loop through each line and sort and append each line into array
     for j in range(len(line)):
         temp = [int(i) for i in line[j].split() if i.isdigit()]
         mergesort(temp)
         array.append(temp)
 
-    print(array)
+    #print(array)
+
+    #insert sorted data value in a different file
+    out_file(array)
+
     # array = [1,5,2,6,9,0,8,3,7]
     # print (array)
     # # mergesort(array, 0, len(array)-1)
@@ -23,13 +28,13 @@ def mergesort(array):
         mid = len(array) // 2
         left = array[:mid]
         right = array[mid:]
-
+        #divide
         mergesort(left)
         mergesort(right)
 
-        i = 0
-        j = 0
-        k = 0
+        i = j = 0 #left and right
+        k = 0 # main array
+        #conquer
         while i < len(left) and j < len(right):
             if left[i] < right[j]:
                 array[k] = left[i]
@@ -48,5 +53,18 @@ def mergesort(array):
             array[k] = right[j]
             j = j + 1
             k = k + 1
+
+
+def out_file(array):
+    out = open("output.txt", "w")
+    out.truncate(0)
+    for i in range(len(array)):
+        write = ""
+        for number in array[i]:
+            write += str(number) + " "
+        write += "\n"
+        out.write(write)
+    #print(write)
+    out.close()
 
 main()
